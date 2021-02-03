c2f6e06995608c8a37dbe01c2d81ed8713fcdf78
Sealnpenguin <sealnpenguin@protonmail.com>
2021-02-03 13:14:12 +0100

    added javaDocs for view and Controllers packages

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/package-info.java
@@ -0,0 +1,5 @@
+package dk.dtu.compute.se.pisd.roborally.view;
+/**
+ * This package holds all classes that is part of the viewLayer
+ * They all handle the User interaction with the GUI and passes this info to the GameController
+ */
