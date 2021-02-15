f7e168cd937d3b34a4bac044eaceffff1aa5a2e2
Martin <s182935@student.dtu.dk>
2021-02-15 10:39:50 +0100

    Ny command kode indsat

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Command.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Command.java
@@ -31,6 +31,51 @@ import java.util.List;
  * @author Ekkart Kindler, ekki@dtu.dk
  *
  */
+
+//************************* OPGAVE V3 *************************//
+public enum Command {
+
+    // This is a very simplistic way of realizing different commands.
+
+    FORWARD("Fwd"),
+    RIGHT("Turn Right"),
+    LEFT("Turn Left"),
+    FAST_FORWARD("Fast Fwd"),
+
+    // XXX Assignment V3
+    OPTION_LEFT_RIGHT("Left OR Right", LEFT, RIGHT);
+
+    final public String displayName;
+
+    // XXX Assignment V3
+    // Command(String displayName) {
+    //     this.displayName = displayName;
+    // }
+    //
+    // replaced by the code below:
+
+    final private List<Command> options;
+
+    Command(String displayName, Command... options) {
+        this.displayName = displayName;
+        this.options = Collections.unmodifiableList(Arrays.asList(options));
+    }
+
+    public boolean isInteractive() {
+        return !options.isEmpty();
+    }
+
+    public List<Command> getOptions() {
+        return options;
+    }
+
+}
+//************************* OPGAVE V3 *************************//
+
+
+
+//************************* Gamle Command *************************//
+/*
 public enum Command {
 
     // This is a very simplistic way of realizing different commands.
@@ -47,3 +92,6 @@ public enum Command {
     }
 
 }
+*/
+//************************* Gamle Command *************************//
+
