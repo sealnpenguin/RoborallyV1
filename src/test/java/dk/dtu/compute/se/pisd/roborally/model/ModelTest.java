752dfdb071d856e3a481a0437a9197c75dd58b27
Martin <s182935@student.dtu.dk>
2021-06-05 13:01:09 +0200

    Checkpoint test

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/model/ModelTest.java
@@ -0,0 +1,36 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ModelTest {
+    @Test
+    public void player_has_to_go_through_checkpoints_in_order(){
+        Board board = new Board(8, 8);
+        Player player1 = new Player(board, "yellow", "player1");
+        player1.RecentCheckpoint(3);
+        Assertions.assertEquals(0, player1.getRecentCheckpoint(),  player1.getName() + " should be 0");
+        player1.RecentCheckpoint(1);
+        Assertions.assertEquals(1, player1.getRecentCheckpoint(),  player1.getName() + " should be 1");
+        player1.RecentCheckpoint(2);
+        player1.RecentCheckpoint(1);
+        Assertions.assertEquals(2, player1.getRecentCheckpoint(),  player1.getName() + " should be 2");
+    }
+    @Test
+    public void player_wins_if_max_checkpoint_reached(){
+        Board board = new Board(8, 8);
+        Player player1 = new Player(board, "yellow", "player1");
+        board.addCheckpoint(true);
+        board.addCheckpoint(true);
+        board.addCheckpoint(true);
+        board.addCheckpoint(true);
+        player1.RecentCheckpoint(1);
+        player1.RecentCheckpoint(2);
+        player1.RecentCheckpoint(3);
+        player1.RecentCheckpoint(4);
+        Assertions.assertEquals(board.getTotalCheckpoints(), player1.getRecentCheckpoint(),  player1.getName() + " should be 4");
+    }
+
+}
